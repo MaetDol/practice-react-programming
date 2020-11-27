a602f1749a08816a3936a1cc917553b2794307fc
Maetdol <rndka9@gmail.com>
2020-11-27 20:59:11 +0900

    서버 API 호출하기
    어제들은 강의인데 오토컴플릿 작동안되는거 수정

--- whois/src/search/container/Search.js
+++ whois/src/search/container/Search.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Row, Col, Typography } from 'antd';
 import Settings from '../component/Settings';
+import SearchInput from './SearchInput';
 
 export default function Search() {
+
   return (
     <>
       <Row justify="end" style={{ padding: 20 }}>
-        <Col><Settings logout={()=>{}}/></Col>
+        <Col>
+          <Settings logout={()=>{}}/>
+        </Col>
       </Row>
       <Row justify="center" style={{ marginTop: 100 }}>
         <Col>
@@ -16,7 +20,9 @@ export default function Search() {
         </Col>
       </Row>
       <Row justify="center" style={{ marginTop: 50 }}>
-        <Col span={12}>검색</Col>
+        <Col span={12}>
+          <SearchInput />
+        </Col>
       </Row>
     </>
   );
